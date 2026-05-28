3c835893fbdd5a079c9d546f65ff09909a93c883
Gowtham45-ai <gowrigowtham2016@gmail.com>
2026-05-28 15:16:30 +0530

    Change login page email to prompt@swais.in

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -57,7 +57,7 @@ export default function LoginPage() {
               
               <p className="text-sm text-gray-300 border-t border-[#2a4575]/50 pt-6 mt-4">
                 Need Help?<br/><br/>
-                Contact us: <a href="mailto:murty.varanasi@swais.in" className="hover:text-white cursor-pointer transition-colors font-medium">murty.varanasi@swais.in</a>
+                Contact us: <a href="mailto:prompt@swais.in" className="hover:text-white cursor-pointer transition-colors font-medium">prompt@swais.in</a>
               </p>
 
               <Link
